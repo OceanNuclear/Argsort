12c87814c864e276db72e9825b27b45b2ef0dc61
OceanNuclear <annoyingamygdala@gmail.com>
2021-07-23 22:22:28 +0100

    final commit before upgrading OS

--- recursive_arg.py
+++ recursive_arg.py
@@ -8,30 +8,43 @@ from pprint import pprint
 from collections import defaultdict
 from numpy import argsort
 
-for array_length in range(15):
-    sorted_version = {pa:argsort(pa) for pa in P(range(array_length))}
-    for key, sorted_key in sorted_version.items():
-        if key==tuple(sorted_key):
-            print(key)
-            print(tuple(sorted_key))
-            print("")
+def is1loop(b):
+    b = ary(b)
+    indices = list(range(len(b)))
+    return (b[b] == indices).all()
 
-    loop_size = {}
-    loop_size_count = defaultdict(int)
-    for key, sorted_key in sorted_version.items():
-        traversed_keys = (key,)
-        # while tuple(sorted_key) not in traversed_keys:
-        while tuple(sorted_key) != traversed_keys[0]:
-            traversed_keys += (key,)
-            key, sorted_key = tuple(sorted_key), sorted_version[tuple(sorted_key)]
+def is2loop(b):
+    return not is1loop(b)
 
-        loop_size[key] = len(traversed_keys)
+if __name__=="__main__":
+    for array_length in range(10):
+        sorted_version = {pa:argsort(pa) for pa in P(range(array_length))}
+        for key, sorted_key in sorted_version.items():
+            if key==tuple(sorted_key):
+                print(key)
+                print(tuple(sorted_key))
+                print("")
 
-    for loop_length in loop_size.values():
-        loop_size_count[loop_length] += 1
-    # if array_length<7:
-    #     pprint(loop_size)
+        loop_size = {}
+        loop_size_count = defaultdict(int)
+        for key, sorted_key in sorted_version.items():
+            traversed_keys = (key,)
+            # while tuple(sorted_key) not in traversed_keys:
+            while tuple(sorted_key) != traversed_keys[0]:
+                traversed_keys += (key,)
+                key, sorted_key = tuple(sorted_key), sorted_version[tuple(sorted_key)]
 
-    print(f"Loop size counts = {loop_size_count}")
-    print("The fraction of self-loops = {}".format(loop_size_count[1]/sum(loop_size_count.values())))
-    input(f"Completd array size of {array_length}. Conclusion: max loop size = {max(loop_size.values())}\n")
+            loop_size[key] = len(traversed_keys)
+            if loop_size[key]==1:
+                assert is1loop(sorted_key), "My is1loop gives false negatives."
+            else:
+                assert not is1loop(sorted_key), "my is1loop checker gives False positives."
+
+        for loop_length in loop_size.values():
+            loop_size_count[loop_length] += 1
+        # if array_length<7:
+        #     pprint(loop_size)
+
+        print(f"Loop size counts = {loop_size_count}")
+        print("The fraction of self-loops = {}".format(loop_size_count[1]/sum(loop_size_count.values())))
+        input(f"Completd array size of {array_length}. Conclusion: max loop size = {max(loop_size.values())}\n")
--- repeated_indexing.py
+++ repeated_indexing.py
@@ -1,5 +1,17 @@
-from itertools import permutations as P
-from numpy import array as ary
-from pprint import pprint
-from collections import defaultdict
-from numpy import argsort
+from recursive_arg import *
+
+if __name__=="__main__":
+    for array_length in range(1,10):
+        sorted_version = {pa:argsort(pa) for pa in P(range(array_length))}
+        same_cd = 0
+        for key, sorted_key in sorted_version.items():
+
+            a = ary(key)
+            b = a[a]
+            c = a[b]
+            d = a[b][b]
+            similarity = c==d
+
+            same_cd += similarity.all()
+        print(f"{same_cd}/{len(sorted_version)} of them has 100% match")
+        input(f"End of {array_length=}")
